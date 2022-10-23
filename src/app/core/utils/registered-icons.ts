dbeb1987aaf3e4754b0fd7cdac9d28f21306612e
Javier Soto <javislop@gmail.com>
2022-10-23 03:18:31 -0400

    remove/organize files

--- src/app/core/utils/registered-icons.ts
+++
@@ -1,4 +0,0 @@
-import { IconDefinition } from '@ant-design/icons-angular';
-import { UserOutline, MenuFoldOutline, MenuUnfoldOutline, FileOutline } from '@ant-design/icons-angular/icons';
-
-export const ICONS: IconDefinition[] = [ UserOutline, MenuFoldOutline, MenuUnfoldOutline, FileOutline ];
