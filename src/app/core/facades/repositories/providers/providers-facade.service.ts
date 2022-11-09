97c3cd6619495aa6e32343065cdd9e15cc1fcb86
Javier Soto <javislop@gmail.com>
2022-11-09 00:14:15 -0500

    add services flow

--- src/app/core/facades/repositories/providers/providers-facade.service.ts
+++ src/app/core/facades/repositories/providers/providers-facade.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ProvidersRepositoryService } from '@infrastructure/repositories/providers/providers-repository.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProvidersFacadeService {
 
-  constructor() { }
+  constructor(private _providersRepositoryService: ProvidersRepositoryService) {
+  }
+
+  getAllProviders(): string {
+    return this._providersRepositoryService.grettingsFromProvidersRepository();
+  }
 }
--- src/app/presentation/pages/providers/providers.component.ts
+++ src/app/presentation/pages/providers/providers.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProvidersFacadeService } from '@core/facades/repositories/providers/providers-facade.service';
 
 @Component({
   selector   : 'app-providers',
   templateUrl: './providers.component.html',
   styleUrls  : [ './providers.component.scss' ]
 })
-export class ProvidersComponent {
+export class ProvidersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _providersFacadeService: ProvidersFacadeService) { }
+
+  ngOnInit(): void {
+    this.getProvidersList();
+  }
+
+  getProvidersList(): void {
+    const provs = this._providersFacadeService.getAllProviders();
+  }
 
 }
